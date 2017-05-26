bb36253faf06ab5d22cc930a193f5e7677ce2a77
Patrick Becich <patbecich@gmail.com>
2017-05-26 11:57:08 -0700

    Unreal Engine building escape grabber method

--- Section_03/BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ Section_03/BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -21,12 +21,14 @@ UGrabber::UGrabber()
 void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
+    FindPhysicsHandleComponent();
+    SetupInputComponent();
+}
 
-	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty"));
-    
+void UGrabber::FindPhysicsHandleComponent(){
     /// Look for attached Physics Handle
     PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
-	if (PhysicsHandle)
+    if (PhysicsHandle)
     {
         //Physics handle found
     }
@@ -34,7 +36,11 @@ void UGrabber::BeginPlay()
     {
         UE_LOG(LogTemp,Error, TEXT("%s missing physics handle component"), *GetOwner()->GetName())
     }
-    /// Look for attached Input Component (only appears at run time)
+}
+
+
+/// Look for attached Input Component (only appears at run time)
+void UGrabber::SetupInputComponent(){
     InputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
     if (InputComponent)
     {
@@ -48,51 +54,65 @@ void UGrabber::BeginPlay()
     {
         UE_LOG(LogTemp,Error, TEXT("%s missing input component"), *GetOwner()->GetName())
     }
+
 }
 
 void UGrabber::Grab(){
     UE_LOG(LogTemp, Error, TEXT("Grab pressed"))
+    
+    // LINE TRACE and see if we reach actors with physics body collision channel set
+    GetFirstPhysicsBodyInReach();
+    // If we hit something, then attach a physics handle
+    ///TODO attach physics handle
 }
 
 void UGrabber::Release(){
     UE_LOG(LogTemp, Error, TEXT("Grab released"))
+    ///TODO release physics handle
+    
 }
 
 // Called every frame
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-	// Get player viewpoint this tick
+    
+    // if the physics handle is attached
+        // move the object we are holding
+    
+
+}
+
+const FHitResult UGrabber::GetFirstPhysicsBodyInReach(){
+    // Get player viewpoint this tick
     FVector PlayerViewPointLocation;
     FRotator PlayerViewPointRotation;
     GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
-        OUT PlayerViewPointLocation,
-        OUT PlayerViewPointRotation
-    );
+                                                               OUT PlayerViewPointLocation,
+                                                               OUT PlayerViewPointRotation
+                                                               );
     //UE_LOG(LogTemp, Warning, TEXT("Location: %s, Rotation: %s"), *PlayerViewPointLocation.ToString(), *PlayerViewPointRotation.ToString());
     // Draw a red trace in the world to visualize
     FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
-    DrawDebugLine(GetWorld(),PlayerViewPointLocation,LineTraceEnd,FColor(255,0,0),false,0.f,0.f,10.f);
     
-
     /// Setup  query parameters
     FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
     
     /// Ray-cast out to reach distance
     FHitResult Hit;
     GetWorld()->LineTraceSingleByObjectType(
-        OUT Hit,
-        PlayerViewPointLocation,
-        LineTraceEnd,
-        FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
-        TraceParameters
+                                            OUT Hit,
+                                            PlayerViewPointLocation,
+                                            LineTraceEnd,
+                                            FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+                                            TraceParameters
                                             
-        );
+                                            );
     /// See what we hit
     AActor* ActorHit = Hit.GetActor();
     if (ActorHit)
     {
         UE_LOG(LogTemp, Warning, TEXT("Line trace hit: %s"), *(ActorHit->GetName()))
     }
+    return FHitResult();
 }
-
